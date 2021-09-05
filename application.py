a088648047946bced83b338baad13cf455df0d9d
Sebastian Ponce <poncegramming@gmail.com>
2021-09-05 15:21:35 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask
+from models.model_back import PredictionNet
+import requests
+import json
 
 app = Flask(__name__)
 
-INFORMATION = {}
+#load model weights
+model_1 = PredictionNet(1, name = "rooms_1")
+model_1.build((None, 1))
+model_1.load_weights('./models/model_1.h5')
 
-# https://url.com/form?params=<info>
-@app.route("/form", methods=["GET"])
+@app.route('/form')
 def get():
-    # This part gets the info from the parameters
-    data = request.args.get("params")
-    INFORMATION['data'] = data
-    # This part redirects the user to response.html after getting the data
-    return redirect("/response")
+    INFORMATION = {"consumption":92, "type": "condo"} # SAMPLE DATA, DELETE LATER
+    return INFORMATION
 
-# This function is just to return the jsonified info and nothing else
-
-def response():
-    # Json should look something like: {'data': (data)}
-    info = jsonify(INFORMATION)
-    return info
+@app.route('/response')
+def prediction():
+    data = requests.get("http://localhost:5000/form")
+    data_txt = json.loads(data.text)
+    forecast = []
+    day = int(model_1.predict((1, data_txt['consumption']))[1])
+    for n in range(5):
+        forecast.append(day)
+        prediction = model_1.predict((1, day))
+        day = float(prediction[1] + prediction[0])
+    return {"prediction": forecast}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
